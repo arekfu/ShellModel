ca030e723b73c0d9c0dfaab6e63d212593395501
Davide Mancusi <davide.mancusi@cea.fr>
2016-07-25 10:14:02 +0200

    Add find* functions to XYTable API

--- src/XYTable.hs
+++ src/XYTable.hs
@@ -2,6 +2,12 @@ module XYTable
 ( xyTable
 , XYTable()
 , (!)
+, findX
+, findY
+, findXY
+, findXIndex
+, findYIndex
+, findXYIndex
 ) where
 
 import qualified Data.Vector.Unboxed as V
@@ -14,3 +20,24 @@ xyTable xs ys = let n = min (V.length xs) (V.length ys)
 
 (!) :: XYTable -> Int -> (Double, Double)
 (!) (XYT xs ys) n = (xs V.! n, ys V.! n)
+
+findXIndex ::(Double -> Bool) ->  XYTable -> Maybe Int
+findXIndex property (XYT xs _) = V.findIndex property xs
+
+findYIndex ::(Double -> Bool) ->  XYTable -> Maybe Int
+findYIndex property (XYT _ ys) = V.findIndex property ys
+
+findXYIndex ::((Double, Double) -> Bool) ->  XYTable -> Maybe Int
+findXYIndex property (XYT xs ys) = V.findIndex property $ V.zip xs ys
+
+findX ::(Double -> Bool) ->  XYTable -> Maybe (Double, Double)
+findX property xy = do i <- findXIndex property xy
+                       return $ xy ! i
+
+findY ::(Double -> Bool) ->  XYTable -> Maybe (Double, Double)
+findY property xy = do i <- findYIndex property xy
+                       return $ xy ! i
+
+findXY::((Double, Double) -> Bool) ->  XYTable -> Maybe (Double, Double)
+findXY property xy = do i <- findXYIndex property xy
+                        return $ xy ! i
